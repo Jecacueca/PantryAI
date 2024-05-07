8ba836cc73ec44314249678f1160a9baab840f0c
Jess Silva Carvalho <jc.carvalho@outlook.com>
2024-05-07 14:32:24 +0100

    model associations and validations for each model completed

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :pantry
   belongs_to :shopping_list
+  validates :name, presence: true
+  validates :quantity, presence: true
+  validates :threshold, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_07_130548) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_07_132623) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -39,6 +39,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_07_130548) do
   create_table "shopping_lists", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_shopping_lists_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -57,4 +59,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_07_130548) do
   add_foreign_key "items", "pantries"
   add_foreign_key "items", "shopping_lists"
   add_foreign_key "pantries", "users"
+  add_foreign_key "shopping_lists", "users"
 end
